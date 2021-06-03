adae389c3b16a69d90930d6dd3a962f31fc161ea
unknown <mfjkd7795@gmail.com>
2021-06-03 06:18:38 -0700

    Store the results in

--- ArthComp.sh
+++ ArthComp.sh
@@ -1,5 +1,7 @@
 echo "Arithmetic Computation and Sorting"
 
+declare -A compute
+
 echo -n "Enter the values for a, b and c"
 read a b c    
 
@@ -8,3 +10,7 @@ uc2=$((a*b+c))
 uc3=$((c+a/b))
 uc4=$((a%b+c))
 echo $uc1 $uc2 $uc3 $uc4
+
+compute+=( ["1"]=$uc1 ["2"]=$uc2 ["3"]=$uc3 ["4"]=$uc4 )
+
+echo ${compute[@]}
