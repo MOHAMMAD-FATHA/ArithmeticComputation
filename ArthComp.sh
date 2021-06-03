dfdd012136fbf05a25d3419cf09cf6558d3a9231
unknown <mfjkd7795@gmail.com>
2021-06-03 06:27:06 -0700

    Read values from dictionary store in array

--- ArthComp.sh
+++ ArthComp.sh
@@ -1,7 +1,7 @@
 echo "Arithmetic Computation and Sorting"
 
 declare -A compute
-
+arr=()
 echo -n "Enter the values for a, b and c"
 read a b c    
 
@@ -11,6 +11,13 @@ uc3=$((c+a/b))
 uc4=$((a%b+c))
 echo $uc1 $uc2 $uc3 $uc4
 
+
 compute+=( ["1"]=$uc1 ["2"]=$uc2 ["3"]=$uc3 ["4"]=$uc4 )
+for i in ${compute[@]}
+do 
+	arr+=($i)
+done 
+
+echo ${arr[@]}
 
 echo ${compute[@]}
