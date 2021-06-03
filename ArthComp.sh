3fb9e0469555fd0f5c565e6f8de2a84819c84950
unknown <mfjkd7795@gmail.com>
2021-06-03 05:38:42 -0700

    Compute a%b+c for inputs

--- ArthComp.sh
+++ ArthComp.sh
@@ -3,8 +3,8 @@ echo "Arithmetic Computation and Sorting"
 echo -n "Enter the values for a, b and c"
 read a b c    
 
-z=$((a +b*c))
-y=$((a*b+c))
-x=$((c+a/b))
-
-echo $z $y $x
+uc1=$((a +b*c))
+uc2=$((a*b+c))
+uc3=$((c+a/b))
+uc4=$((a%b+c))
+echo $uc1 $uc2 $uc3 $uc4
